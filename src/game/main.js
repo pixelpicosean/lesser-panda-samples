16ae0f5ac1504b85128893ef322cdedfa345d26e
Sean Bohan <pixelpicosean@gmail.com>
2016-06-16 09:52:06 +0800

    Make tilemap sample the default one.

--- src/game/main.js
+++ src/game/main.js
@@ -4,28 +4,26 @@ import audio from 'engine/audio';
 import loader from 'engine/loader';
 import PIXI from 'engine/pixi';
 
-// Include device patches you need
-import 'engine/device-patch';
-
 // Loading scene
 import 'game/loading';
 
 // Scenes
-import 'game/samples/asteroids';
+import 'game/samples/actor';
 import 'game/samples/anchor-to-screen';
-import 'game/samples/two-way-move';
+import 'game/samples/asteroids';
+import 'game/samples/face-the-mouse';
+import 'game/samples/physics';
+import 'game/samples/rotate-around';
+import 'game/samples/steering';
 import 'game/samples/tilemap';
 import 'game/samples/timer';
-import 'game/samples/actor';
-import 'game/samples/steering';
-import 'game/samples/face-the-mouse';
-import 'game/samples/rotate-around';
+import 'game/samples/two-way-move';
 
 PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
 
 class Main extends Scene {
   awake() {
-    engine.setScene('Steering');
+    engine.setScene('TilemapSample');
   }
 };
 engine.addScene('Main', Main);
