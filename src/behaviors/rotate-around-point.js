483711c293151b467e917dd0348f9e81e1705644
Sean Bohan <pixelpicosean@gmail.com>
2016-08-25 08:12:31 +0800

    Upgrade engine.

--- src/behaviors/rotate-around-point.js
+++ src/behaviors/rotate-around-point.js
@@ -20,13 +20,13 @@ export default class RotateAroundPoint extends Behavior {
     this.center = Vector.create();
     this.rotation = 0;
   }
-  ready() {
-    this.radius = this.target.position.distance(this.center);
+  awake() {
+    this.radius = this.actor.position.distance(this.center);
   }
   update(_, dt) {
     this.rotation += (dt * this.speed);
 
-    this.target.position.set(this.radius, 0)
+    this.actor.position.set(this.radius, 0)
       .rotate(this.rotation)
       .add(this.center);
   }
